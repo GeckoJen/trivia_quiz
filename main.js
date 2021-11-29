24f913a309797a43f9a3d8985283691abd1dcbc5
jennytaylor <jennytaylor82@gmail.com>
2021-11-29 10:21:59 +0000

    added difficulty level selector

--- main.js
+++ main.js
@@ -8,8 +8,6 @@
 //fetch current category list from api
 //fetch info re no. of questions in category
 
-
-
 let questions = [];
 let questionBlock = document.querySelector(".question");
 let answer1 = document.querySelector("#no1");
@@ -21,13 +19,12 @@ let intro = document.querySelector(".startText");
 let nextQuestionButton = document.querySelector("#nextQuestion");
 let questionAnswerBlock = document.querySelector(".questionBlock");
 let answerGiven = false;
-let scoreBox = document.querySelector("#score")
-let getQuestionButton = document.querySelector("#getQuestions")
-let disappearText = document.querySelector("#disappear")
+let scoreBox = document.querySelector("#score");
+let getQuestionButton = document.querySelector("#getQuestions");
+let disappearText = document.querySelector("#disappear");
 let finalScoreBox = document.querySelector("#finalScoreBox");
 let finalText = document.querySelector("#finalText");
-let playAgain = document.querySelector("#playAgain")
-
+let playAgain = document.querySelector("#playAgain");
 
 // sample url
 // https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple
@@ -36,43 +33,68 @@ let playAgain = document.querySelector("#playAgain")
 let inputNumberQuestions = document.querySelector("#numberQuestions");
 let numQuestions;
 
-let category = document.querySelector("#categoryQuestions")
-let categories = [{name: "General Knowledge", id: 9}, {name: "Books", id: 10}, {name: "Film", id: 11}, {name: "Television", id: 14}, {name: "Science & Nature", id: 17}, {name: "Animals", id: 27}]
+let category = document.querySelector("#categoryQuestions");
+let categories = [
+  { name: "General Knowledge", id: 9 },
+  { name: "Books", id: 10 },
+  { name: "Film", id: 11 },
+  { name: "Television", id: 14 },
+  { name: "Science & Nature", id: 17 },
+  { name: "Animals", id: 27 },
+];
 let categorySelect;
+
+let difficulty = document.querySelector("#difficultyLevel");
+let difficulties = [
+  { name: "Easy", id: "difficulty=easy&" },
+  { name: "Medium", id: "difficulty=medium&" },
+  { name: "Hard", id: "difficulty=hard&" },
+  { name: "Mix", id: "&" },
+];
+let difficultySelect;
 
 //Reuqest questions from API
 async function getQuestions() {
-    numQuestions = inputNumberQuestions.value;
-    categorySelect = categories.filter(function(item) {return item.name === category.value})
-    
-    const response = await fetch(`https://opentdb.com/api.php?amount=${numQuestions}&category=${categorySelect[0].id}&type=multiple`);
+  numQuestions = inputNumberQuestions.value;
+  categorySelect = categories.filter(function (item) {
+    return item.name === category.value;
+  });
+
+  difficultySelect = difficulties.filter(function (item) {
+    return item.name === difficulty.value;
+  });
+
+  const response = await fetch(
+    `https://opentdb.com/api.php?amount=${numQuestions}&category=${categorySelect[0].id}&${difficultySelect[0].id}type=multiple`
+  );
   let data = await response.json();
   questions = data.results;
-  startButton.style.display = "block"
+  startButton.style.display = "block";
   return questions;
 }
 
 //getQuestions();
 
 function populateQuestion() {
-    if(!questions.length) {
-        return
-    } else { 
-  let answersArray = [];
-  answersArray.push(questions[0].correct_answer)
-  answersArray.push(questions[0].incorrect_answers[0])
-  answersArray.push(questions[0].incorrect_answers[1])
-  answersArray.push(questions[0].incorrect_answers[2])
-  let shuffledArray = shuffleArray(answersArray)
+  if (!questions.length) {
+    return;
+  } else {
+    let answersArray = [];
+    answersArray.push(questions[0].correct_answer);
+    answersArray.push(questions[0].incorrect_answers[0]);
+    answersArray.push(questions[0].incorrect_answers[1]);
+    answersArray.push(questions[0].incorrect_answers[2]);
+    let shuffledArray = shuffleArray(answersArray);
 
-  questionBlock.innerHTML = questions[0].question;
-  answer1.innerHTML = shuffledArray[0]
-  answer2.innerHTML = shuffledArray[1]
-  answer3.innerHTML = shuffledArray[2]
-  answer4.innerHTML = shuffledArray[3]
-  displayQuestion();
-  disappearText.style.display = "none";
-}}
+    questionBlock.innerHTML = questions[0].question;
+    answer1.innerHTML = shuffledArray[0];
+    answer2.innerHTML = shuffledArray[1];
+    answer3.innerHTML = shuffledArray[2];
+    answer4.innerHTML = shuffledArray[3];
+    displayQuestion();
+    disappearText.style.display = "none";
+  }
+}
 
 function shuffleArray(array) {
   for (let i = array.length - 1; i >= 0; i--) {
@@ -82,31 +104,31 @@ function shuffleArray(array) {
   return array;
 }
 
-function nextQuestion () {
-  if(questions.length === 0) {
+function nextQuestion() {
+  if (questions.length === 0) {
     finalScoreBox.style.display = "block";
-    finalText.innerHTML = `You have finished the quiz. <br><br> Your final score is ${scoreCounter} out of  ${totalQuestionsAskedCount }.`
+    finalText.innerHTML = `You have finished the quiz. <br><br> Your final score is ${scoreCounter} out of  ${totalQuestionsAskedCount}.`;
     questionAnswerBlock.style.display = "none";
   } else {
-  populateQuestion();
-  console.log(questions)
-  answer1.style.backgroundColor = ""
-  answer2.style.backgroundColor = ""
-  answer3.style.backgroundColor = ""
-  answer4.style.backgroundColor = ""
-  answerGiven = false;
+    populateQuestion();
+    console.log(questions);
+    answer1.style.backgroundColor = "";
+    answer2.style.backgroundColor = "";
+    answer3.style.backgroundColor = "";
+    answer4.style.backgroundColor = "";
+    answerGiven = false;
   }
 }
 
 //Event listeners for buttons
 
-getQuestionButton.addEventListener("click", getQuestions)
+getQuestionButton.addEventListener("click", getQuestions);
 
-startButton.addEventListener("click", populateQuestion)
+startButton.addEventListener("click", populateQuestion);
 
-nextQuestionButton.addEventListener("click", nextQuestion)
+nextQuestionButton.addEventListener("click", nextQuestion);
 
-playAgain.addEventListener("click", resetAll)
+playAgain.addEventListener("click", resetAll);
 
 function displayQuestion() {
   questionAnswerBlock.style.display = "block";
@@ -114,41 +136,36 @@ function displayQuestion() {
   startButton.style.display = "none";
 }
 
-
 let scoreCounter = 0;
 let totalQuestionsAskedCount = 0;
 
-
 //select answer - check if answer correct
 function checkAnswer(event) {
-    while (answerGiven === false) {
-        let correctAnswer = document.createElement('p')
-        correctAnswer.innerHTML = questions[0].correct_answer;
+  while (answerGiven === false) {
+    let correctAnswer = document.createElement("p");
+    correctAnswer.innerHTML = questions[0].correct_answer;
     if (event.target.innerHTML === correctAnswer.innerHTML) {
-        event.target.style.backgroundColor = "green";
-        scoreCounter++
-        
+      event.target.style.backgroundColor = "green";
+      scoreCounter++;
     } else {
-        event.target.style.backgroundColor = "red";
-        event.target.innerHTML += `<br><br>The correct answer was ${correctAnswer.innerHTML}`
+      event.target.style.backgroundColor = "red";
+      event.target.innerHTML += `<br><br>The correct answer was ${correctAnswer.innerHTML}`;
     }
     totalQuestionsAskedCount++;
-    scoreBox.innerHTML = `Score: ${scoreCounter} out of  ${totalQuestionsAskedCount }`
+    scoreBox.innerHTML = `Score: ${scoreCounter} out of  ${totalQuestionsAskedCount}`;
     answerGiven = !answerGiven;
-    console.log(questions[0])
-    questions.shift()
+    console.log(questions[0]);
+    questions.shift();
+  }
+
+  console.log(event.target.innerHTML);
 }
 
-    
-    console.log(event.target.innerHTML)
-    
-}
+answer1.addEventListener("click", checkAnswer);
+answer2.addEventListener("click", checkAnswer);
+answer3.addEventListener("click", checkAnswer);
+answer4.addEventListener("click", checkAnswer);
 
-answer1.addEventListener("click", checkAnswer)
-answer2.addEventListener("click", checkAnswer)
-answer3.addEventListener("click", checkAnswer)
-answer4.addEventListener("click", checkAnswer)
-
-function resetAll () {
+function resetAll() {
   reload = location.reload(true);
 }
